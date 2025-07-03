f0e572420a3703fb7a7b3f6a507e7bfa683c91ee
phat <phat23062019@gmail.com>
2025-07-03 17:35:08 +0700

    Refactor ProgressBarComponent to improve progress handling and update progressColor to 'orange'

--- src/app/interaction/progress-bar.component.ts
+++ src/app/interaction/progress-bar.component.ts
@@ -29,29 +29,57 @@ import { Component, OnInit, OnChanges, Input, Output, EventEmitter, SimpleChange
     `,
   ],
 })
-export class ProgressBarComponent implements OnInit, OnChanges {
+// export class ProgressBarComponent implements OnInit, OnChanges {
+//   @Input() backgroundColor: string = '#ccc';
+//   @Input() progressColor: string = 'tomato';
+//   @Input() progress = 10;
+//   @Output() completed = new EventEmitter<void>();
+
+//   constructor() {}
+
+//   ngOnChanges(changes: SimpleChanges) {
+//     if ('progress' in changes) {
+//       if (typeof changes['progress'].currentValue !== 'number') {
+//         const progress = Number(changes['progress'].currentValue);
+//         if (Number.isNaN(progress)) {
+//           this.progress = 10;
+//         } else {
+//           this.progress = progress;
+//         }
+//       }
+//       if (this.progress >= 100) {
+//         this.completed.emit();
+//       }
+//     }
+//   }
+
+//   ngOnInit() {}
+// }
+
+
+
+export class ProgressBarComponent implements OnInit {
   @Input() backgroundColor: string = '#ccc';
-  @Input() progressColor: string = 'tomato';
-  @Input() progress = 10;
+  @Input() progressColor: string = 'orange';
+
+  private _progress = 10;
+  @Input()
+  set progress(value: number) {
+    const progress = Number(value);
+    if (Number.isNaN(progress)) {
+      this._progress = 10;
+    } else {
+      this._progress = progress;
+    }
+    if (this._progress >= 100) {
+      this.completed.emit();
+    }
+  }
+  get progress(): number {
+    return this._progress;
+  }
   @Output() completed = new EventEmitter<void>();
 
   constructor() {}
-
-  ngOnChanges(changes: SimpleChanges) {
-    if ('progress' in changes) {
-      if (typeof changes['progress'].currentValue !== 'number') {
-        const progress = Number(changes['progress'].currentValue);
-        if (Number.isNaN(progress)) {
-          this.progress = 10;
-        } else {
-          this.progress = progress;
-        }
-      }
-      if (this.progress >= 100) {
-        this.completed.emit();
-      }
-    }
-  }
-
   ngOnInit() {}
 }
